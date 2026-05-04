3cdde97dfa132ef73d90f98a93a94ce2743925da
oogunlan <ogunlanaoluwabunmi@gmail.com>
2026-05-04 16:24:31 +0100

    First rune

--- Quest5/firstrune.go
+++ Quest5/firstrune.go
@@ -1,5 +1,6 @@
 package piscine
 
-func FirstRune(s string) rune {
-	return []rune(s)[0]
+func FirstRune(s string) rune {	
+	// return []rune(s)[0]
+	return rune(s[0])	
 }
--- Quest5/main/main.go
+++ Quest5/main/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"github.com/01-edu/z01"
 	"piscine"
 )
 
 func main() {
-	fmt.Println(piscine.FirstRune("Hello, World!"))
+	z01.PrintRune(piscine.FirstRune("Hello, World!"))
+	z01.PrintRune(piscine.FirstRune("Salut!"))
+	z01.PrintRune(piscine.FirstRune("Ola!"))
+	z01.PrintRune('\n')
 }
