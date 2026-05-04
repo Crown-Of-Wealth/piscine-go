34635985a7135ffa4693ce00dad473d263982a8f
oogunlan <ogunlanaoluwabunmi@gmail.com>
2026-05-04 19:23:07 +0100

    nrune.go

--- Quest5/main/main.go
+++ Quest5/main/main.go
@@ -6,8 +6,14 @@ import (
 )
 
 func main() {
-	z01.PrintRune(piscine.LastRune("Hello, World!"))
-	z01.PrintRune(piscine.LastRune("Salut!"))
-	z01.PrintRune(piscine.LastRune("Ola!"))
+	// z01.PrintRune(piscine.LastRune("Hello, World!"))
+	// z01.PrintRune(piscine.LastRune("Salut!"))
+	// z01.PrintRune(piscine.LastRune("Ola!"))
+	// z01.PrintRune('\n')
+	z01.PrintRune(piscine.NRune("Hello!", 3))
+	z01.PrintRune(piscine.NRune("Salut!", 2))
+	z01.PrintRune(piscine.NRune("Bye!", -1))
+	z01.PrintRune(piscine.NRune("Bye!", 5))
+	z01.PrintRune(piscine.NRune("Ola!", 4))
 	z01.PrintRune('\n')
 }
