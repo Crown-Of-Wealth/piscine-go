96f97897598386d6b5157ccb10c1bd968f94a02a
oogunlan <ogunlanaoluwabunmi@gmail.com>
2026-05-04 19:33:56 +0100

    compare.go

--- Quest5/main/main.go
+++ Quest5/main/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"github.com/01-edu/z01"
+	"fmt"
+	// "github.com/01-edu/z01"
 	"piscine"
 )
 
@@ -10,10 +11,13 @@ func main() {
 	// z01.PrintRune(piscine.LastRune("Salut!"))
 	// z01.PrintRune(piscine.LastRune("Ola!"))
 	// z01.PrintRune('\n')
-	z01.PrintRune(piscine.NRune("Hello!", 3))
-	z01.PrintRune(piscine.NRune("Salut!", 2))
-	z01.PrintRune(piscine.NRune("Bye!", -1))
-	z01.PrintRune(piscine.NRune("Bye!", 5))
-	z01.PrintRune(piscine.NRune("Ola!", 4))
-	z01.PrintRune('\n')
+	// z01.PrintRune(piscine.NRune("Hello!", 3))
+	// z01.PrintRune(piscine.NRune("Salut!", 2))
+	// z01.PrintRune(piscine.NRune("Bye!", -1))
+	// z01.PrintRune(piscine.NRune("Bye!", 5))
+	// z01.PrintRune(piscine.NRune("Ola!", 4))
+	// z01.PrintRune('\n')
+	fmt.Println(piscine.Compare("Hello!", "Hello!"))
+	fmt.Println(piscine.Compare("Salut!", "lut!"))
+	fmt.Println(piscine.Compare("Ola!", "Ol"))
 }
